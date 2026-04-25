91703e461934593c84fdc2c4a745e788225cda1d
UXDev25 <uxdev25@gmail.com>
2026-04-25 17:11:02 +0200

    feat: made heroes and abilities saveable into a .Json

---
+++ Ultimate_HeroEngine/Logic/ProgramEngine/HeroStorage.cs
@@ -0,0 +1,17 @@
+﻿using Ultimate_HeroEngine.Hierarchy;
+using Ultimate_HeroEngine.Hierarchy.Entities.Heroes;
+
+namespace Ultimate_HeroEngine.Logic.ProgramEngine;
+
+public static class HeroStorage
+{
+    private static Team _heroTeamList = new Team();
+    public static Team HeroTeamList
+    {
+        get => _heroTeamList;
+        set
+        {
+            _heroTeamList = value;
+        }
+    }
+}
